59bbafd3723aa4f7b864e2476fd7309cff6d5523
Andrey Lepikhov <a.lepikhov@postgrespro.ru>
2020-09-09 09:40:17 +0500

    Add sql/local_foreign_table.sql

---
+++ sql/local_foreign_table.sql
@@ -0,0 +1,12 @@
+DROP EXTENSION IF NOT EXISTS postgres_fdw CASCADE;
+DROP TABLE IF EXISTS l CASCADE;
+
+CREATE EXTENSION postgres_fdw;
+CREATE SERVER loopback FOREIGN DATA WRAPPER postgres_fdw;
+CREATE USER MAPPING FOR PUBLIC SERVER loopback;
+
+CREATE TABLE l(a int);
+CREATE FOREIGN TABLE f(a int) SERVER loopback OPTIONS (table_name 'l');
+
+--COPY (SELECT * FROM generate_series(1,100) as gs) TO '/home/andrey/1.txt';
+--COPY f FROM '/home/andrey/1.txt';
